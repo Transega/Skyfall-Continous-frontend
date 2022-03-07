18f25052c1a362b1c5feb013464d49daa8e04ffe
korykorir <stephenkorykorir@gmail.com>
2022-03-07 13:31:44 +0300

    Added the map component

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,17 +1,11 @@
 import { StyledCard } from './styles/Card.styled'
 import Calendar from './Calendar'
+import MapDeck from './Map'
 
 export default function Card() {
   return (
     <StyledCard layout = "column">
-      <div>
-        <h2>MAP</h2>
-        <p>WE ARE GOING TO PUT OUR MAP HERE</p>
-      </div>
-
-      <div>
-        <img src={`./images/illustration-grow-together.svg`} alt='' />
-      </div>
+      <MapDeck/>
       
     </StyledCard>
   )
---
+++ frontend/src/components/Map.js
@@ -0,0 +1,29 @@
+import React,{useState} from 'react';
+import Map, {Marker} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = "pk.eyJ1Ijoia29yeWtvcmlyMTIzIiwiYSI6ImNsMGdqcjdybTEzcTczanBybHU5anN6bnUifQ.rIUBT4fmSIwMuwN_vtUznw"
+
+
+function MapDeck(){
+
+    const [viewState, setViewState] = useState({
+        longitude: 36.543,
+        latitude: 0.453,
+        zoom: 10
+    })
+  return (
+
+    <Map
+      {...viewState}
+      onMove={evt => setViewState(evt.viewState)}
+      
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Marker longitude={36.543} latitude={0.453} color="red" />
+    </Map>
+  )
+}
+
+export default MapDeck
