fba8619edc95e0c7c29a7b5ede845ceb6800d55b
korykorir <stephenkorykorir@gmail.com>
2022-03-09 09:10:28 +0300

    All changes

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import Map, {Marker} from 'react-map-gl';
+import Map, {Marker,Source, Layer} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MAPBOX_TOKEN = "pk.eyJ1Ijoia29yeWtvcmlyMTIzIiwiYSI6ImNsMGdqcjdybTEzcTczanBybHU5anN6bnUifQ.rIUBT4fmSIwMuwN_vtUznw"
@@ -21,6 +21,23 @@ function MapDeck(){
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={MAPBOX_TOKEN}
     >
+        <Source 
+        id='testSource' 
+        type='raster' 
+        tiles={["mapbox://styles/korykorir123/cl0hzgbrv000515o9adccekbk"]} 
+        tileSize={256}>
+        
+        
+        <Layer 
+            id ='simple-tiles'
+            type = 'raster'
+            source = 'raster-tiles'
+            minzoom ={0} 
+            maxzoom =  {22 }
+            />
+       </Source>
+
+        
       <Marker longitude={36.543} latitude={0.453} color="red" />
     </Map>
   )
