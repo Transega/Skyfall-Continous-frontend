b1ce803a5eaa6485d296e54d00efe17177daf4d3
Transega <collinsasega.ca@gmail.com>
2022-04-08 10:20:51 +0300

    added table

--- frontend/src/components/SideBarMenu.js
+++ frontend/src/components/SideBarMenu.js
@@ -17,7 +17,82 @@ import customstyle from './styles/table.css'
  
  </style>
  
-const SideBarMenu = ({ADM1Geojson, adm1RsData,mapRef, showimage}) => {
+const SideBarMenu = ({ADM1Geojson, adm1RsData,mapRef, showimage,productSelected}) => {
+
+
+  // var it = ["cat","dog","chicken","pig"].sort().reduce(
+  //   (acc, item, i, arr) => acc.concat(
+  //     arr.slice(i + 1).map(_item => [item, _item])
+  //   ),
+  // [])
+  // console.log(it)
+
+
+  
+
+  const NdviData = [
+    {
+        "Time": 1617264611524,
+        "NDVI": 0.4804017856942116
+    },
+    {
+        "Time": 1617264608338,
+        "NDVI": 0.48811391852795977
+    }]
+
+
+    
+    const humanReadableDate = (time)=> {
+      if (time !=='SeasonNDVI'){
+        var date = new Date(time);
+          
+        var Day = date.getDate().toString().length ==2 ? date.getDate() : `0${date.getDate()}`
+
+
+        var year = date.getFullYear()
+        var month = date.getMonth() + 1
+        var formatedMonth = month.toString().length ==2 ? month : `0${month}`
+        // console.log(date, month,Day, 'y', year)
+
+        
+        //template string
+
+        var actualDate = `${Day}-${formatedMonth}-${year}`
+
+      } else {
+        actualDate = "Period Average"
+      }
+      if (time ==="SeasonNDMI"){
+        actualDate = "Period Average"
+      }
+      
+        
+
+        return actualDate
+      }
+
+
+
+  const DateData = (Array) => {
+    const tableData = Array.map((item) =>{
+      // console.log(item.Time)
+      return (
+
+        
+        
+        <td>{humanReadableDate(item.Time)}</td>
+        
+       
+      )
+    })
+
+    // const dynamicdata = adm1RsData.time_series
+
+    // dynamicdata.map((data) => data.Time)
+
+    return tableData
+
+  }
 
   
 
@@ -31,18 +106,22 @@ const SideBarMenu = ({ADM1Geojson, adm1RsData,mapRef, showimage}) => {
       <Container>
     <Logo/>
     </Container>
-      <Chart adm1RsData={adm1RsData} showimage={showimage}/>
+      <Chart adm1RsData={adm1RsData} showimage={showimage} productSelected={productSelected}/>
       <div className='Table'>
      <table>
        <tr>
-         <th>Date</th>
+         <th>StartDate</th>
+         <th>EndDate</th>
          <th>Stage</th>
-         <th>Condition</th>
+         <th>Crop Condition</th>
        </tr>
        <tr>
-         <td>2022-01-01</td>
+         {DateData(NdviData)}
          <td>Emergence</td>
-         <td>Good</td>
+         <td>Average</td>
+
+         {/* <td>Test</td> */}
+         {/* <td>Test</td> */}
          
        </tr>
      </table>
