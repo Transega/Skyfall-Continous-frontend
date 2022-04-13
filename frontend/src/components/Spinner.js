f6867d06e6dace409ef9d4ad5eac8d217b20fc58
Transega <collinsasega.ca@gmail.com>
2022-04-13 09:17:11 +0300

    added text on the loading component

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -4,9 +4,16 @@ import customstyle from './styles/table.css'
 
 export default function LoadingSpinner() {
   return (
+    <div>
     <div className="spinner-container">
       <div className="loading-spinner">
+          
       </div>
+      
+      
+      
+    </div>
+    <h3 className="spinner-text">Please wait while loading.. .</h3>
     </div>
   );
 }
