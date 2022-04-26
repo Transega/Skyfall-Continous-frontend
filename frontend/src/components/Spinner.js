8f85b811a1b23a6a1e57256b9fafdedb9063f6ce
Transega <collinsasega.ca@gmail.com>
2022-04-26 22:31:17 +0300

    fixed spiner

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -4,11 +4,6 @@ import customstyle from './styles/table.css'
 
 export default function LoadingSpinner() {
   return (
-<<<<<<< HEAD
-    <div className="spinner-container">
-      <div className="loading-spinner">
-      </div>
-=======
     <div>
     <div className="spinner-container">
       <div className="loading-spinner">
@@ -19,7 +14,6 @@ export default function LoadingSpinner() {
       
     </div>
     <h3 className="spinner-text">Please wait while loading.. .</h3>
->>>>>>> 59047174758939a9b8710182739ab9b1231537d7
     </div>
   );
 }
